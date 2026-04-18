c019443fe64698b3bf24746b63b9a8bcc9ec203c
Codzign <codzign@gmail.com>
2026-04-18 09:50:50 +0200

    chore: remove compiled src/ artifacts from repo, add to .gitignore

--- src/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { detectChanges, getImpactRadius, } from './analysis.js';
-export type { ChangedFile, OverlayRisk, RiskLevel, ChurnBucket, DetectChangesInput, DetectChangesResult, ImpactReport, ImpactInput, HistoricalCouplingEntry, } from './analysis.js';
-//# sourceMappingURL=index.d.ts.map
